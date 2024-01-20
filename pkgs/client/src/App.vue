c377a3e5fb89a525962ee5163b86874b0f204330
nekosu <liao.hy@outlook.com>
2024-01-20 14:13:40 +0800

    feat: block when disconnect
    add heart beat test

--- pkgs/client/src/App.vue
+++ pkgs/client/src/App.vue
@@ -7,17 +7,20 @@ import {
   VAppBar,
   VAppBarNavIcon,
   VBtn,
+  VCard,
+  VDialog,
   VList,
   VListItem,
   VMain,
   VNavigationDrawer,
+  VOverlay,
   VSpacer,
   VToolbarTitle
 } from 'vuetify/components'
 
 import { service } from '@/stores/service'
 
-const { connect, disconnect, version } = service
+const { connect, connecting, disconnect, version } = service
 
 const showDrawer = ref(false)
 
@@ -29,19 +32,26 @@ onMounted(() => {
 <template>
   <v-app class="bg-gray-200">
     <v-app-bar>
-      <v-app-bar-nav-icon
+      <!-- <v-app-bar-nav-icon
         icon="mdi-menu"
         @click.stop="showDrawer = !showDrawer"
-      ></v-app-bar-nav-icon>
+      ></v-app-bar-nav-icon> -->
       <v-toolbar-title>maa-z</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn v-if="version" @click="disconnect"> 已连接 - {{ version }} </v-btn>
-      <v-btn v-else @click="connect"> 未连接 </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="showDrawer">
+    <!-- <v-navigation-drawer v-model="showDrawer">
       <v-list> </v-list>
-    </v-navigation-drawer>
+    </v-navigation-drawer> -->
+
+    <v-dialog :model-value="!version" class="w-1/3" persistent>
+      <v-card class="p-4">
+        <div class="flex justify-center">
+          <v-btn @click="connect" :loading="connecting"> 连接 MaaHttp </v-btn>
+        </div>
+      </v-card>
+    </v-dialog>
 
     <v-main>
       <router-view></router-view>
--- pkgs/client/src/stores/service.ts
+++ pkgs/client/src/stores/service.ts
@@ -1,16 +1,25 @@
-import { ApiServerError, api } from '@maaz/schema'
+import { api } from '@maaz/schema'
 import { ref } from 'vue'
 
 function useService() {
   const version = ref<string | null>(null)
+  const connecting = ref(false)
+
+  setInterval(() => {
+    connect()
+  }, 30 * 1000)
 
   const connect = async () => {
-    const { return: ver } = await api.MaaVersion()
-    if (ver.length === 40) {
-      version.value = ver.slice(0, 8)
-    } else {
-      version.value = ver
-    }
+    connecting.value = true
+    try {
+      const { return: ver } = await api.MaaVersion()
+      if (ver.length === 40) {
+        version.value = ver.slice(0, 8)
+      } else {
+        version.value = ver
+      }
+    } catch (_) {}
+    connecting.value = false
   }
 
   const disconnect = () => {
@@ -19,6 +28,7 @@ function useService() {
 
   return {
     version,
+    connecting,
     connect,
     disconnect
   }
